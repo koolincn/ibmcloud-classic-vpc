d9468ffc4f1e18c8a6e9e56170ac471b90c1878d
koolincn <zhutingsh@hotmail.com>
2021-07-13 13:35:49 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,14 @@
+variable "iaas_classic_username" {
+   type = "string"
+}
+variable "iaas_classic_api_key" {
+   type = "string"
+}
+
 provider "ibm" {
   region           = var.region
   generation       = 2
+  iaas_classic_username = var.iaas_classic_username
+  iaas_classic_api_key  = var.iaas_classic_api_key
   ibmcloud_timeout = var.ibmcloud_timeout
 }
