77ead4c28fa21395be9c544632c8515abfae969a
koolincn <zhutingsh@hotmail.com>
2021-11-22 15:36:35 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -35,18 +35,32 @@ variable "subnet_zone" {
   default = "us-south-1"
 }
 
-variable "ssh_keyname" {}
+variable "ssh_keyname" {
+  default = ""
+  description = "The sshkey name tat will be used to create vsis in the VPC. Required for users to specify."
+}
 
 variable vpc_instance_count {
   default = 1
 }
 
+variable "vpc_vsi_instance_name" {
+  default     = "zhut-test"
+  description = "The name of the virtual server instance in the VPC. Required for users to specify."
+}
+variable "vpc_vsi_profile" {
+  default     = "cx2-2x4"
+  description = "The profile of compute CPU and memory resources to use when creating the virtual server instance in VPC. To list available profiles, run the `ibmcloud is instance-profiles` command."
+}
+
 variable "baremetal_hostname" {
   default = "bms-demo"
+  description = "The name of the Bare Metal server in the Classic environment. Required for users to specify."
 }
 
 variable "vsi_hostname" {
   default = "vsi-demo"
+  description = "The name of the virtual server instance in the Classic environment. Required for users to specify."
 }
 
 variable "computers_datacenter" {
